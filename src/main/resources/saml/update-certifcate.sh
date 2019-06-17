3e19103d4d62bc72efebad734a30550e4d8ec605
Marcos <devilcius@gmail.com>
2019-06-17 12:18:16 +0200

    Certificate update integrated within pom.xml

--- src/main/resources/saml/update-certifcate.sh
+++ src/main/resources/saml/update-certifcate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  
 IDP_HOST=idp.ssocircle.com
 IDP_PORT=443
 CERTIFICATE_FILE=ssocircle.cert
-KEYSTORE_FILE=samlKeystore.jks
+KEYSTORE_FILE="${SCRIPT_DIR}/samlKeystore.jks"
 KEYSTORE_PASSWORD=buhbuh121
 
 openssl s_client -host $IDP_HOST -port $IDP_PORT -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $CERTIFICATE_FILE
